6e2cf979772aafaecf2867ab815bff87370042ab
ilker ALTIN <ilkerobi@gmail.com>
2019-03-13 16:56:19 +0100

    Add link to footer

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -6,7 +6,12 @@ export default () => {
       <div className="footer-copyright">
         <div className="container">
           © 2019 SPA Daily News - ilker ALTIN
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a
+            className="grey-text text-lighten-4 right"
+            href="http://bit.ly/2F9uFa3"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Source Code
           </a>
         </div>
